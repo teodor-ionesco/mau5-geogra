2ded50a582925ba262b831b41a426098d706191b
teodor-ionesco <31542280+teodor-ionesco@users.noreply.github.com>
2018-12-01 14:47:27 +0200

    Update global.blade.php

--- resources/views/global.blade.php
+++ resources/views/global.blade.php
@@ -20,7 +20,7 @@
 		 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script> 
 		 <script>
 		 	$(document).ready(function(){
-		 		$("a[link='yes']").attr('href', this.next().attr('src'));
+		 		$("a[link='yes']").attr('href', $(this).next('img').attr('src'));
 		 	});
 		 	
 		 </script>
