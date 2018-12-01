a293de21be3cd04d00ec2a0d2ff0000f8b978f4f
teodor-ionesco <31542280+teodor-ionesco@users.noreply.github.com>
2018-12-01 18:13:48 +0200

    Update StaticHandler.php

--- app/Http/Controllers/StaticHandler.php
+++ app/Http/Controllers/StaticHandler.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Facades\Support\Storage;
+use Illuminate\Support\Facades\Storage;
 
 class StaticHandler extends Controller
 {
